3e5ce8db9e6aef9875d312a130b27360bc223dfa
Kahmali Rose <krose72205@gmail.com>
2014-01-21 19:28:02 -0500

    Added base activity

---
+++ src/com/overthink/mbtace/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.overthink.mbtace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Parent for all MBTAce activities
+ */
+public class BaseActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
