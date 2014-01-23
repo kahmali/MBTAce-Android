a16e4605ae2bf96659e20387fb32acb7c40743bb
steven57146 <steven.gatto001@umb.edu>
2014-01-23 18:19:36 -0500

    added model class stub for Transit Details

---
+++ src/com/overthink/mbtace/ui/TransitDetails.java
@@ -0,0 +1,10 @@
+package com.overthink.mbtace.ui;
+
+import com.google.gson.annotations.*;
+
+/**
+ * Model class for Transit Details that contains additional info on the steps of the trip
+ */
+public class TransitDetails {
+
+}
