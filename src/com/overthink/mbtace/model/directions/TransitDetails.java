e9b866f4fa3f47c7bcd029c8466d0d967490a18b
Dave <dkraytsberg@gmail.com>
2014-01-28 14:37:56 -0500

    transit details stub model classes

--- src/com/overthink/mbtace/model/directions/TransitDetails.java
+++ src/com/overthink/mbtace/model/directions/TransitDetails.java
@@ -14,63 +14,66 @@ public class TransitDetails {
     private int headway,numStop;
 
     /**
-     * @return
-     */
-    public int getNumStop() {
-        return numStop;
-    }
-
-    /**
-     * @return
-     */
-    public int getHeadway() {
-        return headway;
-    }
-
-    /**
-     * @return
-     */
-    public Line getLine() {
-        return line;
-    }
-
-    /**
-     * @return
+     * @return arrival Stop object for this part of the trip.
      */
     public Stop getArrivalStop() {
         return arrivalStop;
     }
 
+
     /**
-     * @return
+     * @return departure Stop object for this part of the trip.
      */
     public Stop getDepartureStop() {
         return departureStop;
     }
 
+
     /**
-     * @return
+     * @return the arrival time for this leg of the journey
      */
     public Time getArrivalTime() {
         return arrivalTime;
     }
 
+
     /**
-     * @return
+     * @return the departure time for this leg of the journey
      */
     public Time getDepartureTime() {
         return departureTime;
     }
 
+
     /**
-     * @return
+     * @return the direction in which to travel on this line, usually will be the terminus station.
      */
     public String getHeadsign() {
         return headsign;
     }
 
 
+    /**
+     * @return the expected number of seconds between departures from the same stop at this time.
+     */
+    public int getHeadway() {
+        return headway;
+    }
 
 
+    /**
+     * @return the number of stops in this step, counting the arrival stop, but not the departure stop.
+     */
+    public int getNumStop() {
+        return numStop;
+    }
+
+
+    /**
+     * @return  information about the transit Line object used in this step
+     */
+    public Line getLine() {
+        return line;
+    }
 
 }
