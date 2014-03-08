4a00f739f78bbdfc63bcab0327c1eb5df7a0666e
steven57146 <steven.gatto001@umb.edu>
2014-03-08 16:26:43 -0500

    Completed GSON parse into models from JSON for trip planner

---
+++ src/com/overthink/mbtace/ui/TripPlannerAdapter.java
@@ -0,0 +1,12 @@
+package com.overthink.mbtace.ui;
+
+import android.widget.ArrayAdapter;
+import com.overthink.mbtace.model.directions.Route;
+
+/**
+ * Created by Steve on 3/8/14.
+ */
+public class TripPlannerAdapter extends ArrayAdapter<Route> {
+
+
+}
